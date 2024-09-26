bccfce99f3d507064228a278ba0d238fe504c307
sldang <spencerdang727@gmail.com>
2024-09-25 19:33:12 -0700

    Second commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom';
+
+export default function Header({
+    heading,
+    paragraph,
+    linkName,
+    linkUrl="#"
+}){
+    return(
+        <div className="mb-10">
+            <div className="flex justify-center">
+                <h1 className="text-4xl">Pixure</h1>
+            </div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                {heading}
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600 mt-5">
+            {paragraph} {' '}
+            <Link to={linkUrl} className="font-medium text-black hover:text-gray-500">
+                {linkName}
+            </Link>
+            </p>
+        </div>
+    )
+}
---
+++ src/constants/formFields.js
@@ -0,0 +1,97 @@
+const loginFields=[
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired:true,
+        placeholder:"Email address"   
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"   
+    }
+]
+
+const signupFields=[
+    {
+        labelText:"First Name",
+        labelFor:"firstname",
+        id:"firstname",
+        name:"firstname",
+        type:"text",
+        autoComplete:"firstname",
+        isRequired:true,
+        placeholder:"First Name"   
+    },
+    {
+        labelText:"Last Name",
+        labelFor:"lastname",
+        id:"lastname",
+        name:"lastname",
+        type:"text",
+        autoComplete:"lastname",
+        isRequired:true,
+        placeholder:"Last Name"   
+    },
+    {
+        labelText:"Username",
+        labelFor:"username",
+        id:"username",
+        name:"username",
+        type:"text",
+        autoComplete:"username",
+        isRequired:true,
+        placeholder:"Username"   
+    },
+    {
+        labelText:"Zip Code",
+        labelFor:"zipcode",
+        id:"zipcode",
+        name:"zipcode",
+        type:"numbers",
+        autoComplete:"zipcode",
+        isRequired:true,
+        placeholder:"Zip Code"   
+    },
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired:true,
+        placeholder:"Email address"   
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"   
+    },
+    {
+        labelText:"Confirm Password",
+        labelFor:"confirm-password",
+        id:"confirm-password",
+        name:"confirm-password",
+        type:"password",
+        autoComplete:"confirm-password",
+        isRequired:true,
+        placeholder:"Confirm Password"   
+    }
+]
+
+export {loginFields,signupFields}
