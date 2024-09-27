d7ebe990ebae87516e81540c395b3c90a23beb99
Dzermak <makdzer@gmail.com>
2024-09-26 23:53:12 -0700

    Tried to vite
    didnt work

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,52 +3,92 @@ import { signupFields } from "../constants/formFields"
 import FormAction from "./FormAction";
 import Input from "./Input";
 
-const fields=signupFields;
-let fieldsState={};
+const fields = signupFields;
+let fieldsState = {};
 
-fields.forEach(field => fieldsState[field.id]='');
+fields.forEach(field => fieldsState[field.id] = '');
 
-export default function Signup(){
-  const [signupState,setSignupState]=useState(fieldsState);
+export default function Signup() {
+  const [signupState, setSignupState] = useState(fieldsState);
 
-  const handleChange=(e)=>setSignupState({...signupState,[e.target.id]:e.target.value});
+  const handleChange = (e) => setSignupState({ ...signupState, [e.target.id]: e.target.value });
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    console.log(signupState)
+    console.log(signupState['email-address'])
     createAccount()
   }
 
   //handle Signup API Integration here
-  const createAccount=()=>{
+  const createAccount = async() => {
+    
+    try{
+      
+      const response = fetch("https://cs4800-server.onrender.com/api/User", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(
+          {
+            firstName: signupState['firstname'],
+            lastName: signupState['lastname'],
+            nickname: signupState['username'],
+            email: signupState['email-address'],
+            zipcode: signupState['zipcode'],
+            password: signupState['password'],
+            friendsList: 0,
+            followList: 0,
+            karma: 0,
+            communityIDs: 0,
+            posts: 0,
+            age: 0,
+            searchTags: 0,
+            postAndFlagsTags: 0,
+            profilePic: 0,
+            parentAccount: 0,
+            parentAccountID: 0,
+            childAccount: 0,
+            childAccountID: 0,
 
+          }
+        )
+        
+      })
+
+      if(response.ok){
+        
+      }else{
+        console.log("failed")
+      }
+    }catch (error){
+      console.log("error")
+    }
   }
 
-    return(
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-        <div className="">
+  return (
+    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <div className="">
         {
-                fields.map(field=>
-                        <Input
-                            key={field.id}
-                            handleChange={handleChange}
-                            value={signupState[field.id]}
-                            labelText={field.labelText}
-                            labelFor={field.labelFor}
-                            id={field.id}
-                            name={field.name}
-                            type={field.type}
-                            isRequired={field.isRequired}
-                            placeholder={field.placeholder}
-                    />
-                
-                )
-            }
-          <FormAction handleSubmit={handleSubmit} text="Signup" />
-        </div>
+          fields.map(field =>
+            <Input
+              key={field.id}
+              handleChange={handleChange}
+              value={signupState[field.id]}
+              labelText={field.labelText}
+              labelFor={field.labelFor}
+              id={field.id}
+              name={field.name}
+              type={field.type}
+              isRequired={field.isRequired}
+              placeholder={field.placeholder}
+            />
 
-         
+          )
+        }
+        <FormAction handleSubmit={handleSubmit} text="Signup" />
+      </div>
 
-      </form>
-    )
+
+
+    </form>
+  )
 }
