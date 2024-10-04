e9351aa8f0d36924458838669957dec959c370d9
Dzermak <makdzer@gmail.com>
2024-10-03 21:51:58 -0700

    ../home

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header"
 import Login from "../components/Login"
-import Home from "/Home"
+import Home from "../Home"
 
 export default function LoginPage(){
     return(
