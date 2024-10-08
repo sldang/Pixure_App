204640e67a5d28f55e7832947539c328bdabd624
Dzermak <makdzer@gmail.com>
2024-10-08 14:22:09 -0700

    change import

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,4 +1,4 @@
-import { AuthContext } from "../contexts/AuthContext";
+import { AuthContext } from ".contexts/authContext";
 import { useContext } from "react";
 
 export const useAuthContext = () => {
