6e592952628ed1dafa763e69b7a891931fd53d2a
aerovvs <siagejustin@gmail.com>
2024-10-10 22:41:12 -0700

    Sidebar

--- src/components/HomeComponents/Sidebar.jsx
+++ src/components/HomeComponents/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaHome, FaSearch, FaCompass, FaEnvelope, FaBell, FaUser, FaUsers, FaSignOutAlt } from 'react-icons/fa'; // import icons from react-icons to use in navbar
 import { useNavigate } from 'react-router-dom'; // import useNavigate from react-router-dom for navigation
 
 const Sidebar = () => {
