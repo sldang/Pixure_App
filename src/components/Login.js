3a770ed1f78e42a30f5f3499293578e1bc1e7996
Dzermak <makdzer@gmail.com>
2024-10-10 23:01:19 -0700

    links

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ export default function Login() {
     // Handle Login API Integration here
     const authenticateUser = async () => {
         try {
-            const response = await fetch("http://localhost:8000/api/login", { 
+            const response = await fetch("https://cs4800-server.onrender.com/api/login", { 
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
