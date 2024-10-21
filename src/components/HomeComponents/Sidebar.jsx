5a571dea8c9ceeb4a82225f2ea3de5f86db6801d
Dzermak <makdzer@gmail.com>
2024-10-21 03:31:32 -0700

    log

--- src/components/HomeComponents/Sidebar.jsx
+++ src/components/HomeComponents/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaHome, FaSearch, FaCompass, FaEnvelope, FaBell, FaUser, FaUsers, FaSignOutAlt } from 'react-icons/fa'; // import icons from react-icons to use in navbar
 import { useNavigate } from 'react-router-dom'; // import useNavigate from react-router-dom for navigation
-import { useLogout } from '../hooks/useLogout';
+import { useLogout } from '../../hooks/useLogout';
 
 const Sidebar = () => {
   const navigate = useNavigate();
