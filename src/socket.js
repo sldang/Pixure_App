8f473730e9eaddd6738a31a6dc77e67d423641fc
Dzermak <makdzer@gmail.com>
2024-10-28 07:42:58 -0700

    no more tests socket

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000'); 
-export default socket;
+// const socket = io('http://localhost:3000'); 
+// export default socket;
