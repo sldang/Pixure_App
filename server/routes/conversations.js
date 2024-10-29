88d8aa193e966c5d2e96ccf392f9679220b4d9f6
Dzermak <makdzer@gmail.com>
2024-10-29 16:14:37 -0700

    conversations shoudl work

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -5,45 +5,34 @@ const router = express.Router();
 const cors = require("cors");
 
 
-// router.post("/", async (req, res) => {
-//     const { userEmail, otherEmail } = req.body;
-//     console.log("User Email:", userEmail);
-//     console.log("Other Email:", otherEmail);
-//     const user = await User.findOne({ email: userEmail })
-//     const recipient = await User.findOne({ email: otherEmail })
-//     console.log("User Email:", user.email);
-//     console.log("Other Email:", recipient.email);
-//     if (!user || !recipient) {
-//         console.log("Invalid credentials");
-//         return res.status(404).json({ message: "User not found" });
-//     } else {
-
-//         const senderId = user._id
-//         const recipientId = recipient._id
-//         const newConversation = new Conversation({
-//             members: [senderId, recipientId],
-//         });
-//         try {
-//             const savedConversation = await newConversation.save();
-//             res.status(200).json(savedConversation);
-//         } catch (err) {
-//             res.status(500).json(err);
-//         }
-//     }
-// });
-
 router.post("/", async (req, res) => {
-    const newConversation = new Conversation({
-        members: [req.body.senderId, req.body.receiverId],
-    });
+    const { userEmail, otherEmail } = req.body;
+    console.log("User Email:", userEmail);
+    console.log("Other Email:", otherEmail);
+    const user = await User.findOne({ email: userEmail })
+    const recipient = await User.findOne({ email: otherEmail })
+    console.log("User Email:", user.email);
+    console.log("Other Email:", recipient.email);
+    if (!user || !recipient) {
+        console.log("Invalid credentials");
+        return res.status(404).json({ message: "User not found" });
+    } else {
 
-    try {
-        const savedConversation = await newConversation.save();
-        res.status(200).json(savedConversation);
-    } catch(err){
-        res.status(500).json(err);
+        const senderId = user._id
+        const recipientId = recipient._id
+        const newConversation = new Conversation({
+            members: [senderId.toString(), recipientId.toString()],
+        });
+        try {
+            const savedConversation = await newConversation.save();
+            res.status(200).json(savedConversation);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 });
+
+
 
 // get conv of a user
 router.get("/:userId", async (req, res) => {
