53a53dd034dde70604dcf56aac2be67e1e7ca11f
ericshen010 <ericshen010@gmail.com>
2024-10-30 06:19:36 -0700

    router change
    router change

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -10,14 +10,14 @@ router.post('/', async (req, res) => {
       time: req.body.time,
     });
     const savedPost = await newPost.save();
-    res.status(201).json(savedPost);
+    res.status(200).json(savedPost);
   } catch (error) {
     res.status(500).json({ error: 'Error saving post' });
   }
 });
 
 // Get all posts
-router.get('/', async (req, res) => {
+router.get('/:postId', async (req, res) => {
   try {
     const posts = await Post.find({});
     res.status(200).json(posts);
