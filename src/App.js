90b7dfbb2a53e8116019d8620a9a1f5898247cb1
Spencer Dang <spencerdang727@gmail.com>
2024-10-30 12:48:41 -0700

    updated profile link

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
             <Route path="/explore" element={user ? <ExplorePage/> : <Navigate to="/login"/>} />
             <Route path="/communities" element={user ? <CommunitiesPage/> : <Navigate to="/login"/>} />
             <Route path="/editprofile" element={<EditProfile/>} />
+            <Route path="/profile" element={<HomePage/>} />
           </Routes>
         </div>
       </BrowserRouter>
