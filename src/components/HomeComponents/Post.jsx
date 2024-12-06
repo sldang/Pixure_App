a4a8edc8f1fc5eda2d0c54825380bc267a1cced5
Spencer Dang <124112090+sldang@users.noreply.github.com>
2024-12-05 21:05:32 -0800

    Update Post.jsx

--- src/components/HomeComponents/Post.jsx
+++ src/components/HomeComponents/Post.jsx
@@ -42,7 +42,7 @@ const Post = ({
           )}
         </div>
       </div>
-      <div className="mt-[20px] mb-[20px] text-left">
+      <div className="mt-[20px] mb-[20px] pl-[50px]">
         {img && <img src={img} alt="Uploaded content" className="w-full h-auto mb-[10px]" />}
         <span>{content}</span>
       </div>
