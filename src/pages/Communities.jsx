a35bc2b35c969dd5f0a20bdcb85a68907bd9e057
Dzermak <makdzer@gmail.com>
2024-12-09 18:49:22 -0800

    Update Communities.jsx

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useCommunityContext } from '../contexts/CommunityContext';
 import CommunityModal from './CommunityModal';
 
 const Communities = () => {
   const { state } = useCommunityContext(); // Access state from the context
   const { joinedCommunities } = state; // Destructure joined communities
+  const parsedData = JSON.parse(localStorage.getItem('user'));
+  const nickname = parsedData && parsedData.user ? parsedData.user.nickname : null;
 
   const [selectedCommunity, setSelectedCommunity] = useState(null); // Track the selected community for the modal
   const [isModalOpen, setIsModalOpen] = useState(false); // Modal state
