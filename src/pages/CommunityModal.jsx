879af05ce7d08dbced2953509d28b41fe4fe2e85
Dzermak <makdzer@gmail.com>
2024-12-11 21:51:44 -0800

    Update CommunityModal.jsx

--- src/pages/CommunityModal.jsx
+++ src/pages/CommunityModal.jsx
@@ -16,6 +16,7 @@ const CommunityModal = ({ community, onClose }) => {
           imageUrl: community.imageUrl || 'https://via.placeholder.com/100x100',
           members: community.communityMembers.length || 0,
         }));
+        console.log("updating posts");
         setPosts(formattedData); // Update state
       })
       .catch((error) => console.error('Error fetching communities:', error));
