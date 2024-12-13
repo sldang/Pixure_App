c645b9cc3152c88979a2d870cbbc1cd45df4f801
Dzermak <makdzer@gmail.com>
2024-12-12 22:59:15 -0800

    Update communities.js

--- server/routes/communities.js
+++ server/routes/communities.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const Community = require('./models/Community');
+const Community = require('../models/Community');
 
 //add messages
 router.use(
